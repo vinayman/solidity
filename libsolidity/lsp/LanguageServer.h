ec900f358aed8a706b1469d2baaa3016815383de
chriseth <chris@ethereum.org>
2021-12-16 13:48:23 +0100

    LSP for solcjs

--- libsolidity/lsp/LanguageServer.h
+++ libsolidity/lsp/LanguageServer.h
@@ -57,6 +57,17 @@ public:
 	/// @return boolean indicating normal or abnormal termination.
 	bool run();
 
+	/// Run a single iteration of processing inputs and generating outputs.
+	/// To be used when we are not in control of the event loop.
+	/// @returns false if the process is supposed to terminate.
+	bool runIteration();
+
+	/// @returns true if the server has not terminated yet, false otherwise.
+	bool isRunning() const noexcept
+	{
+		return m_state != State::ExitRequested && m_state != State::ExitWithoutShutdown && !m_client.closed();
+	}
+
 private:
 	/// Checks if the server is initialized (to be used by messages that need it to be initialized).
 	/// Reports an error and returns false if not.
